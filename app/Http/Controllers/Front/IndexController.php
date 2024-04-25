f84463478f71c9e997d1c0c54f4500eeb854323b
Lin Khant Ko <linkhantko1@gmail.com>
2024-04-25 22:59:07 +0630

    lkk

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('front.home.index');
+        $brands = Brand::all();
+        return view('front.home.index', compact('brands'));
     }
 }
